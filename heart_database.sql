3603b03a0a8a815aa106a8407ab7be9490dfb12a
sofya365 <146061770+sofya365@users.noreply.github.com>
2023-12-14 19:01:54 +0400

    new

--- heart_database.sql
+++ heart_database.sql
@@ -3,13 +3,13 @@ CREATE DATABASE IF NOT EXISTS `heart` /*!40100 DEFAULT CHARACTER SET utf8mb4 COL
 
 USE `heart`;
 
-CREATE TABLE patients (
+CREATE TABLE IF NOT EXISTS patients (
   id integer NOT NULL PRIMARY KEY auto_increment,
   age int NOT NULL,
   sex VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE medicalexam (
+CREATE TABLE IF NOT EXISTS medicalexam (
 id INT REFERENCES patients(id),
 ChestPainType VARCHAR(100) NOT NULL,
 RestingBP INT NOT NULL,
@@ -20,3 +20,18 @@ MaxHR INT NOT NULL,
 ExerciseAngina VARCHAR(100) NOT NULL,
 Oldpeak DECIMAL(10,1) NOT NULL
 );
+
+INSERT INTO patients(id, age, sex) VALUES
+(1, 40, M)
+
+INSERT INTO medicalexam(id, ChestPainType, RestingBP, Cholesterol, FastingBS, MaxHR, ExerciseAngina, Oldpeak) VALUES
+(1, ATA, 140, 289, 0, Normal, 172, N, 0)
+
+ALTER TABLE patients
+    ADD CONSTRAINT patients_pk PRIMARY KEY (id);
+
+ALTER TABLE medicalexam
+    ADD CONSTRAINT medicalexam_pk PRIMARY KEY (id);
+
+ALTER TABLE patients
+    ADD CONSTRAINT fk_patients_id FOREIGN KEY (id) REFERENCES medicalexam(id);

e691dc71b2a7571fa1285da38a337bfb1c72b3a6
sofya365 <146061770+sofya365@users.noreply.github.com>
2023-12-15 17:07:40 +0400

    new

--- heart_database.sql
+++ heart_database.sql
@@ -18,7 +18,9 @@ FastingBS TINYINT,
 RestingECG VARCHAR(100) NOT NULL,
 MaxHR INT NOT NULL,
 ExerciseAngina VARCHAR(100) NOT NULL,
-Oldpeak DECIMAL(10,1) NOT NULL
+Oldpeak DECIMAL(10,1) NOT NULL,
+ST_Slope VARCHAR(100) NOT NULL,
+HeartDisease TINYINT
 );
 
 INSERT INTO patients(id, age, sex) VALUES
